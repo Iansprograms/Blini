c83b13284190ee0e81c7aabefa13331cdb825fff
ian.moss@ncl.ac.uk <ian.moss@ncl.ac.uk>
2023-07-06 15:05:50 +0100

    Initial Commit

---
+++ Blini/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Blini
+//
+//  Created by ian.moss on 06/07/2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
